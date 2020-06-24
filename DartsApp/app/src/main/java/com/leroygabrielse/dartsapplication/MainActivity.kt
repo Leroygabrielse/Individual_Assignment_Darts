bd6083796a0485f33e7c460b71b237882525fedb
L.Gabrielse <leroy.gabrielse@hva.nl>
2020-06-24 14:29:05 +0200

    Layout game

--- DartsApp/app/src/main/java/com/leroygabrielse/dartsapplication/MainActivity.kt
+++ DartsApp/app/src/main/java/com/leroygabrielse/dartsapplication/MainActivity.kt
@@ -1,17 +1,29 @@
 package com.leroygabrielse.dartsapplication
 
+import android.annotation.SuppressLint
+import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pre_game)
+        setContentView(R.layout.activity_main)
 
         initViews()
     }
+    @SuppressLint("ResourceAsColor")
     private fun initViews(){
-
+        btnStart.setOnClickListener {
+            Snackbar.make(tvBeginScore, "Works", Snackbar.LENGTH_LONG).show()
+        }
+        btnSets.setOnClickListener {
+            
+        }
     }
 }
