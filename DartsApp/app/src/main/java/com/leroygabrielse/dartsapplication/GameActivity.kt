996272c1d63f377d6376ff46492a589f23c3958f
L.Gabrielse <leroy.gabrielse@hva.nl>
2020-06-24 15:53:25 +0200

    numberclicks

--- DartsApp/app/src/main/java/com/leroygabrielse/dartsapplication/GameActivity.kt
+++ DartsApp/app/src/main/java/com/leroygabrielse/dartsapplication/GameActivity.kt
@@ -3,6 +3,7 @@ package com.leroygabrielse.dartsapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_game.*
 
 const val GAMESTATS_REQUEST_CODE = 400
@@ -16,11 +17,66 @@ class GameActivity : AppCompatActivity() {
         initViews()
     }
     private fun initViews(){
+
+        tvHintP1.visibility = View.INVISIBLE
+        tvHintP2.visibility = View.INVISIBLE
+
         btnStats.setOnClickListener {view->
             val intent = Intent(this, StatsActivity::class.java)
             startActivityForResult(intent,
                 GAMESTATS_REQUEST_CODE
             )
+        }
+        btnEnter.setOnClickListener {
+
+        }
+        btnOne.setOnClickListener {
+            enterNumber("1")
+        }
+        btnTwo.setOnClickListener {
+            enterNumber("2")
+        }
+        btnThree.setOnClickListener {
+            enterNumber("3")
+        }
+        btnFour.setOnClickListener {
+            enterNumber("4")
+        }
+        btnFive.setOnClickListener {
+            enterNumber("5")
+        }
+        btnSix.setOnClickListener {
+            enterNumber("6")
+        }
+        btnSeven.setOnClickListener {
+            enterNumber("7")
+        }
+        btnEight.setOnClickListener {
+            enterNumber("8")
+        }
+        btnNine.setOnClickListener {
+            enterNumber("9")
+        }
+        btnZero.setOnClickListener {
+            enterNumber("0")
+        }
+
+        btnBackspace.setOnClickListener {
+            if(tvScoreInput.text.toString().length > )
+            val newScore = tvScoreInput.text.toString().substring(0, tvScoreInput.text.toString().length - 1)
+            tvScoreInput.setText(newScore)
+        }
+
+    }
+    fun enterNumber(number: String){
+        val score = tvScoreInput.text.toString()
+        if (score.toInt()<99){
+            if (score == "0"){
+                tvScoreInput.setText(number)
+            }
+            else {
+                tvScoreInput.setText(score + number)
+            }
         }
     }
 }
